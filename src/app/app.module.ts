1036382c570f4faf36cc36c2d4771ab81038aac9
Srinivas <srinivas1993.ee@outlook.com>
2018-02-19 23:41:42 +0530

    added angularfire2 module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { firebaseConfig } from "./config";
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +26,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/config.ts
@@ -0,0 +1,8 @@
+export const  firebaseConfig = {
+    apiKey: "AIzaSyAtrmcRZZ7AXTEdnsLcEOc82kDafduZaVk",
+    authDomain: "admoney-v1.firebaseapp.com",
+    databaseURL: "https://admoney-v1.firebaseio.com",
+    projectId: "admoney-v1",
+    storageBucket: "",
+    messagingSenderId: "97878924701"
+};
